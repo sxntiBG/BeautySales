7dd34038a5d2b158018ab3ee6eb0735622188bf7
SxntiBG <zapataospinasantiago8@gmail.com>
2023-12-14 15:32:42 -0500

    Agrego las correciones

--- SistemaVentaBlazor/Shared/ProductoDTO.cs
+++ SistemaVentaBlazor/Shared/ProductoDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,16 @@ namespace SistemaVentaBlazor.Shared
     public class ProductoDTO
     {
         public int IdProducto { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido.")]
         public string? Nombre { get; set; }
         public int? IdCategoria { get; set; }
         public string? DescripcionCategoria { get; set; }
+
+        [Required(ErrorMessage = "El stock es requerido.")]
         public int? Stock { get; set; }
+
+        [Required(ErrorMessage = "El precio es requerido.")]
         public decimal? Precio { get; set; }
     }
 }
--- SistemaVentaBlazor/Shared/UsuarioDTO.cs
+++ SistemaVentaBlazor/Shared/UsuarioDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,16 @@ namespace SistemaVentaBlazor.Shared
     public class UsuarioDTO
     {
         public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido.")]
         public string? NombreApellidos { get; set; }
+
+        [Required(ErrorMessage = "El correo es requerido.")]
         public string? Correo { get; set; }
         public int IdRol { get; set; }
         public string? rolDescripcion { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida.")]
         public string? Clave { get; set; }
     }
 }
